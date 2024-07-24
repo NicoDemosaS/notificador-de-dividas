9f21abc3186057b60afa088d0ee470027fcfcbce
Gabriel <102257502+NicoDemosaS@users.noreply.github.com>
2024-07-24 12:33:29 -0300

    Selenium funcionando no chrome

--- whatizap.py
+++ whatizap.py
@@ -8,19 +8,27 @@ mensagem = '''
 Nome:\n
 Divida:
 '''
+
 urllib.parse.quote(mensagem)
 
 # Inicializa o navegador
 navegador = webdriver.Firefox()
-#navegador.get("https://web.whatsapp.com/")
+navegador.get("https://web.whatsapp.com/")
 
-input('CONFIRMAR: ')
+input('CONFIRMAR 1 ')
 xpath = "/html/body/div[1]/div/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span"
 
 # Agora você pode enviar a mensagem
 numero = 556791260268
 
 link = f"https://web.whatsapp.com/send?phone={numero}&text={mensagem}"
-#navegador.get(link)
-navegador.find_element_xpath(xpath).send_keys(Keys.ENTER)
+navegador.get(link)
 
+input('CONFIRMAR 2 ')
+
+campo_mensagem = navegador.find_element_by_xpath(xpath)
+campo_mensagem.send_keys(Keys.ENTER)
+
+input("CONFIRMAR 3 ")
+
+navegador.quit()
---
+++ whatzapchrome.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome()
+
+xpath = "/html/body/div[1]/div/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p"
+
+driver.get("https://web.whatsapp.com/")
+
+input("CONFIRMAR 1")
+numero = "556791260268"
+mensagem = "Olá, estou enviando esta mensagem via Selenium!"
+link = f"https://web.whatsapp.com/send?phone={numero}&text={mensagem}"
+driver.get(link)
+
+driver.implicitly_wait(5.5)
+
+
+input('Confirmar 2')
+driver.find_element(By.XPATH, xpath).send_keys(Keys.ENTER)
+
+input("Confirmar 3")
+driver.quit()
+
