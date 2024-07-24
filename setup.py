23d8ff898169b17a83749c99f9f39c1dee38ea40
Gabriel <102257502+NicoDemosaS@users.noreply.github.com>
2024-07-24 14:05:45 -0300

    setup.py feito, parte da gui do usuario

---
+++ setup.py
@@ -0,0 +1,122 @@
+import os
+from main import Clientes
+
+def iniciar_whatssap():
+    # seleniumstuff();
+    # Open the web whatssap browser
+    # Wait for registration
+    # Ok message
+    pass
+
+def setup():
+    os.system('cls')
+    print('Iniciando Programa!')
+    print('-=-'*10)
+    print('1 - Inserir Cliente')
+    print('2 - Deletar Cliente')
+    print('3 - Aumenta Divida')
+    print('4 - Diminuir Divida')
+    print('5 - Zerar Divida')
+    print('6 - Mostrar Inf de um Cliente')
+    print('7 - Mostrar Database')
+    print("-=-"*10)
+
+    opcao = int(input('Escolha: '))
+    if opcao == 1:
+        inserir_cliente()
+    if opcao == 2:
+        deletar_cliente()
+    if opcao == 3:
+        adicionar_divida()
+    if opcao == 4:
+        diminuir_divida()
+    if opcao == 5:
+        zerar_divida()
+    if opcao == 6:
+        mostrar_cliente()
+    if opcao == 7:
+        mostrar_todos_clientes()
+
+        
+def inserir_cliente():  # Opçao 1
+    # Add client to the database
+    # Add Name and Number
+    
+    # Verify the name
+    name = input('nome a adicionar:')
+    
+    # Verify Number
+    number = input(f'numero a adicionar a {name}:')
+    
+    cliente = Clientes(name, number)
+    Clientes.inserir_cliente_db(cliente)
+    
+    print(f'Inserindo Cliente: {name} com numero {number} ao Banco de Dados')
+    input('CONFIRMAR: ')
+    
+    setup()
+
+def deletar_cliente(): # Opçao 2
+    # Delete Cliente from the Database
+    
+    # Verify if the name actual exists in the Database
+    name = input('Cliente a Deletar: ')
+    print(f'Deletando Cliente: {name} do Banco de Dados')
+    
+    Clientes.deletar_cliente(name)
+    
+    input('Confirmar: ')
+    setup()
+
+def adicionar_divida(): # Opçao 3
+    # Add a debt to the Client
+    # Send a Message to the client
+
+    # Verify if is the actual person u want to add debt
+    name = input('Nome do Cliente: ')
+    divida = int(input('Divida a aumentar: '))
+    print(f'Adicionando divida de: {divida} ao Cliente: {name}')
+    Clientes.aumentar_divida(name, divida)
+
+    # Send a message to client warning about the new debt
+    input('Confirmar: ')
+    setup()
+
+def diminuir_divida(): # Opçao 4
+    # Remove debt from the client
+    # Send a Message to the client
+
+    # Verify if is the actual person u want to remove debt
+    name = input('Nome do Cliente: ')
+    dividapaga = int(input('Divida Paga: '))
+    print(f'Diminuindo divida: {dividapaga} do Cliente: {name}')
+    Clientes.diminuir_divida(name, dividapaga)
+
+    input('Confirmar: ')
+    setup()
+
+def zerar_divida(): # Opçao 5
+    # Remove all debt from the client
+    # Send message to the client
+    name = input('Nome do Cliente: ')
+    print(f'Zerando divida do Cliente: {name}')
+    Clientes.zerar_divida(name)
+
+    input('Confirmar: ')
+    setup()
+
+def mostrar_cliente(): # Opçao 6
+    # Show inf about one Client
+    print('Mostrando Informaçoes do Cliente:')
+
+
+def mostrar_todos_clientes(): # Opçao 7
+    # Show all the clients
+    print('Mostrando dados do Banco de Dados')
+    Clientes.mostrar_db()
+
+    input('Confirmar: ')
+    setup()
+
+if __name__ == '__main__':
+    setup()
--- whatzapchrome.py
+++ whatzapchrome.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
 driver = webdriver.Chrome()
 
 xpath = "/html/body/div[1]/div/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p"
@@ -13,10 +12,11 @@ input("CONFIRMAR 1")
 numero = "556791260268"
 mensagem = "Olá, estou enviando esta mensagem via Selenium!"
 link = f"https://web.whatsapp.com/send?phone={numero}&text={mensagem}"
+
 driver.get(link)
 
-driver.implicitly_wait(5.5)
-
+cookies = driver.get_cookies()
+print(cookies)
 
 input('Confirmar 2')
 driver.find_element(By.XPATH, xpath).send_keys(Keys.ENTER)
